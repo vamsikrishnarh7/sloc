a7994bc2a847e148e9465396d702a765bf1354b3
Durga Vamsi Krishna <vamsikrishnaacpt@gmail.com>
2023-04-24 20:27:46 +0530

    add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthContextProvider } from './context/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename='/sloc'>
   <AuthContextProvider>
   <App />
   </AuthContextProvider>
