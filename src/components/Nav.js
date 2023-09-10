ced5eb27842c37c03871ee8d730144a9fb80f4a1
Durga Vamsi Krishna <vamsikrishnaacpt@gmail.com>
2023-09-11 02:31:29 +0530

    added menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,22 +14,68 @@ let Nav = () => {
             <div className="nav-right">
               <ul className="nav-items-list">
                 <li className="nav-items nav-item1">
-                  <Link to="/" className="navlinks">Home</Link>
+                  <Link to="/" className="navlinks">
+                    Home
+                  </Link>
                 </li>
                 <li className="nav-items nav-item1">
-                  <Link to="/resources" className="navlinks">Resources</Link>
+                  <Link to="/resources" className="navlinks">
+                    Resources
+                  </Link>
                 </li>
                 <li className="nav-items nav-item2">
-                  <Link to="/events" className="navlinks">Events</Link>
+                  <Link to="/events" className="navlinks">
+                    Events
+                  </Link>
                 </li>
                 <li className="nav-items nav-item3">
-                  <Link to="/clubs" className="navlinks">Clubs</Link>
+                  <Link to="/clubs" className="navlinks">
+                    Clubs
+                  </Link>
                 </li>
                 <li className="nav-items nav-item3">
-                  <Link to="/about" className="navlinks">About us</Link>
-                </li>                
+                  <Link to="/about" className="navlinks">
+                    About us
+                  </Link>
+                </li>
               </ul>
             </div>
+          </div>
+          <div className="menu-container">
+            <label className="hamburger-menu">
+              <input type="checkbox" />
+            </label>
+            <aside className="sidebar">
+              <nav>
+                <ul className="nav-items-list">
+                  <li className="nav-items nav-item1">
+                    <Link to="/" className="navlinks">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-items nav-item1">
+                    <Link to="/resources" className="navlinks">
+                      Resources
+                    </Link>
+                  </li>
+                  <li className="nav-items nav-item2">
+                    <Link to="/events" className="navlinks">
+                      Events
+                    </Link>
+                  </li>
+                  <li className="nav-items nav-item3">
+                    <Link to="/clubs" className="navlinks">
+                      Clubs
+                    </Link>
+                  </li>
+                  <li className="nav-items nav-item3">
+                    <Link to="/about" className="navlinks">
+                      About us
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </aside>
           </div>
         </nav>
       </header>
